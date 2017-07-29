d2a2a08eddba9ae5fe437d4d006424ecdef5567d
Fafa87 <fafafft@gmail.com>
2017-07-29 02:20:55 +0200

    wyłączenie konsoli i test pełnoekranowości

--- Domki/main.cpp
+++ Domki/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!", sf::Style::Fullscreen);
 	sf::CircleShape shape(200.f);
 	shape.setFillColor(sf::Color::Green);
 
@@ -11,6 +11,20 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Escape:
+					window.create(sf::VideoMode(500, 500), "SFML works!", sf::Style::Default);
+					break;
+				case sf::Keyboard::Return:
+					window.create(sf::VideoMode(500, 500), "SFML works!", sf::Style::Fullscreen);
+					break;
+				}
+				break;
+			}
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}

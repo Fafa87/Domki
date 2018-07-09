90a3aa5f3030d8cb2ca548cf9475b194b0772bea
JusDus <jusdus@wp.pl>
2018-07-09 23:29:05 +0200

    #38 Wyświetlanie planszy jako listy list Metody: wstawianie obiektu + testy TODO: wyjątek we wstawianiu obiektu!

--- Edytor/plansza.py
+++ Edytor/plansza.py
@@ -4,6 +4,13 @@ class Plansza:
         self.h = h
         self.w = w
         self.d = d #dokładność
+        self.siatka = []
+        a = self.w * ['.']
+        for i in range(self.h):
+            self.siatka.append(list(a))
+
+    def __repr__(self):
+        return str(self.siatka)
 
     def wielkosc_planszy(self):
         px_h = self.h*self.d
@@ -20,13 +27,32 @@ class Plansza:
         x = w*self.d + 0.5*self.d
         return y, x
 
+    def wstawianie_obiektu(self,h,w,obiekt):
+        if self.siatka[h][w] == '.':
+            self.siatka[h][w] = obiekt
+            return True
+        elif self.siatka[h][w] != '.':
+            return False
+
+
+
+
+plansza = Plansza(3,3)
+print(plansza)
 
 
 
 
 
 
-"""Wstawianie obiektu (tekstu) do podanego pola (H, W) (rzuca wyjątek gdy H < 0 lub W < 0 lub gdy pole zajęte).
+
+
+
+
+
+
+
+"""
 Zapytanie o zawartość pola (H, W) lub przez punkt (Y, X).
 Możliwość wyczyszczenia pola (H, W) lub przez punkt (Y, X).
 Aktualny rozmiar planszy w polach i w rozmiarze.
--- Edytor/test_plansza.py
+++ Edytor/test_plansza.py
@@ -3,30 +3,42 @@ from plansza import Plansza
 
 
 class TestPlansza(unittest.TestCase):
-
     def test_nowa_plansza(self):
-        nowa_plansza = Plansza(1,1)
+        nowa_plansza = Plansza(1, 1)
         self.assertEqual(1, nowa_plansza.d)
 
     def test_wielkosc_planszy(self):
-        nowa_plansza = Plansza(1,1,10)
-        self.assertEqual((10,10), nowa_plansza.wielkosc_planszy())
-        nowa_plansza = Plansza(2,3,10)
-        self.assertEqual((20,30), nowa_plansza.wielkosc_planszy())
+        nowa_plansza = Plansza(1, 1, 10)
+        self.assertEqual((10, 10), nowa_plansza.wielkosc_planszy())
+        nowa_plansza = Plansza(2, 3, 10)
+        self.assertEqual((20, 30), nowa_plansza.wielkosc_planszy())
 
     def test_punkt_na_pole(self):
-        nowa_plansza = Plansza(10,10,10)
-        self.assertEqual((0,0), nowa_plansza.punkt_na_pole(5,5))
-        nowa_plansza = Plansza(10,10,10)
-        self.assertEqual((1,2), nowa_plansza.punkt_na_pole(15,25))
-        nowa_plansza = Plansza(10,10,10)
-        self.assertEqual((1,2), nowa_plansza.punkt_na_pole(18,23))
+        nowa_plansza = Plansza(10, 10, 10)
+        self.assertEqual((0, 0), nowa_plansza.punkt_na_pole(5, 5))
+        nowa_plansza = Plansza(10, 10, 10)
+        self.assertEqual((1, 2), nowa_plansza.punkt_na_pole(15, 25))
+        nowa_plansza = Plansza(10, 10, 10)
+        self.assertEqual((1, 2), nowa_plansza.punkt_na_pole(18, 23))
 
     def test_pole_na_punkt(self):
-        nowa_plansza = Plansza(10,10,10)
-        self.assertEqual((5,5), nowa_plansza.pole_na_punkt(0,0))
+        nowa_plansza = Plansza(10, 10, 10)
+        self.assertEqual((5, 5), nowa_plansza.pole_na_punkt(0, 0))
+
+    def test_wstawianie_obiektu(self):
+        nowa_plansza = Plansza(3, 3)
+        """self.assertRaises(("Nie ma takiego pola"), nowa_plansza.wstawianie_obiektu(-1, 11, "obiekt"))"""
+        nowa_plansza.wstawianie_obiektu(1, 1, "obiekt")
+        self.assertEqual(False, nowa_plansza.wstawianie_obiektu(1, 1, "obiekt"))
+        nowa_plansza = Plansza(3, 3)
+        self.assertEqual(True, nowa_plansza.wstawianie_obiektu(0, 0, "obiekt"))
+
+    def test_wyglad_planszy(self):
+        nowa_plansza = Plansza(3, 3)
+        self.assertEqual("[['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]", str(nowa_plansza))
 
 
+"""Wstawianie obiektu (tekstu) do podanego pola (H, W) (rzuca wyjątek gdy H < 0 lub W < 0 lub gdy pole zajęte)."""
 
 if __name__ == '__main__':
     unittest.main()

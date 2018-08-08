ae1f7275d52e08cbbacc111e9ccbccc9288edef7
JusDus <jusdus@wp.pl>
2018-08-08 08:53:53 +0200

    #39 wczytanie domku na planszę nazwa mapy = nazwa pliku Przedziwnej konstrukcji funkcja liczby (zwraca liczbę graczy, dróg i wypisuje unikalne drogi) poprawiony test_wczytaj_domek (porównuje parametry domków, a nie same domki)

--- Edytor/mapa.py
+++ Edytor/mapa.py
@@ -1,4 +1,5 @@
 from plansza import Plansza
+import os
 
 
 class Mapa(Plansza):
@@ -12,20 +13,19 @@ class Mapa(Plansza):
         return "{}: {}\n {}".format(self.nazwa_mapy, self.wielkosc_planszy_pola(), self.prezentacja())
 
     def statystyki(self):
-        for i in
-        return "{}\n".format(self.prezentacja)
+
+        for domek in self.lista_domkow:
+
+
+            return "{}\n".format(self.prezentacja)
 
     # Wypisać się w konsoli: plansza, drogi, statystyki
-    # (#domków, #graczy, odległość między graczami).
+    # (#domków, #graczy, odległość między graczami minimalna, rozmiar zajmowany przez domki).
 
 
     def wczytaj_txt(self, plik):
         slownik_domki = {}
-        #TODO
-        """
-         jak wczytujemy z jakiegos pliku to ustawmy
-         nazwa_mapy na nazwę tego pliku (bez rozszerzenia)
-        """
+        self.nazwa_mapy = os.path.splitext(os.path.basename(plik))[0]
         with open(plik, 'r') as mapa:
             for domek in range(int(mapa.readline())):
                 nr_domku, l_info = mapa.readline().split(" ")
@@ -35,31 +35,57 @@ class Mapa(Plansza):
                 slownik_domki[nr_domku] = lista_info
         return slownik_domki
 
-    def wczytaj_domek(self, para_nazwa_cechy_domka):
+    def wczytaj_domki(self, para_nazwa_cechy_domka):
         """'1': [['koordynaty', '300', '400'], ['drogi', '1', '6'], ['gracz', '1']],"""
         for key in para_nazwa_cechy_domka:
             domek = Domek(key)
             for i in para_nazwa_cechy_domka[key]:
                 if i[0] == 'koordynaty':
-                    domek.x = int(i[1])
-                    domek.y = int(i[2])
+                    domek.y = int(i[1])
+                    domek.x = int(i[2])
                 if i[0] == 'drogi':
-                    lista_drog = [j for j in i[1:]]
-                    for j in lista_drog:
+                    drogi = i[2:]
+                    for j in drogi:
                         domek.dodaj_sasiada(int(j))
                 if i[0] == 'gracz':
                     domek.gracz = int(i[1])
             self.lista_domkow.append(domek)
         return self.lista_domkow
 
+
+    def domek_na_plansze(self):
+        for domek in self.lista_domkow:
+            h, w = self.punkt_na_pole(domek.y, domek.x)
+            self.wstaw(h, w, domek)
+
     def usun_domek(self, nazwa):
         if nazwa in self.lista_domkow:
             self.lista_domkow.remove(nazwa)
 
+    def liczby(self):
+        lista_graczy = []
+        for domek in self.lista_domkow:
+            if domek.gracz:
+                lista_graczy.append(domek.gracz)
+        liczba_graczy = len(set(lista_graczy))
+        lista_drog = []
+        lista_drog_krotka = []
+        for domek in self.lista_domkow:
+            for sasiad in domek.lista_sasiadow:
+                lista_drog.append([int(domek.numer), sasiad])
+        for listy in lista_drog:
+            listy.sort()
+        for listy in lista_drog:
+            lista = tuple(listy)
+            lista_drog_krotka.append(lista)
+        lista_drog_krotka = list(set(lista_drog_krotka))
+        liczba_drog = len(lista_drog_krotka)
+        return "Liczba graczy: {}\nLiczba dróg: {}\nDrogi:{}\n".format(liczba_graczy, liczba_drog, lista_drog_krotka)
 
 
 
-class Domek:
+
+class Domek(object):
 
     def __init__(self, numer, gracz=None):
         self.numer = numer
@@ -79,11 +105,13 @@ class Domek:
 
 if __name__ == '__main__':
     mapa = Mapa(1, 1, "Pierwsza plansza")
-
-    wynik = mapa.wczytaj_domek({'1': [['koordynaty', '300', '400'], ['drogi', '1', '6'], ['gracz', '1']],
-                              '2': [['koordynaty', '1100', '400'], ['drogi', '1', '4'], ['gracz', '2']]})
+    wynik = mapa.wczytaj_domki({'1': [['koordynaty', '300', '400'], ['drogi', '1', '2'], ['gracz', '1']],
+                              '2': [['koordynaty', '1100', '400'], ['drogi', '1', '1'], ['gracz', '2']],
+                                '3': [['koordynaty', '300', '400'], ['drogi', '2', '1', '4']]})
     for i in wynik:
-        print((i.x, i.y, i.lista_sasiadow, i.gracz))
+        print((i.y, i.x, i.lista_sasiadow, i.gracz))
+    print(mapa.liczby())
+
 
 
 """Statyczna metoda Mapa.z_pliku(sciezka) tworząca obiekt.
@@ -94,4 +122,6 @@ Zwracać: listę graczy obecnych na mapie, listę istniejących dróg.
 Zapisanie się do pliku jako mapa Domków, którą potem można uruchomić w grze.
 Metoda obiektu mapa.zapisz(sciezka).
 Sprawdzenie poprawności mapy (co najmniej dwóch graczy, między dowolnymi dwoma domkami da się przejść)."""
+
+
 
--- Edytor/test_mapa.py
+++ Edytor/test_mapa.py
@@ -9,20 +9,20 @@ class TestMapa(unittest.TestCase):
     def test_wczytaj_txt(self):
         mapka = Mapa(10, 10, "nowa_mapa", 100)
         self.assertEqual({'1': [['koordynaty', '300', '400'], ['drogi', '1', '6'], ['gracz', '1']],
-                          '2': [['koordynaty', '1100', '400'], ['drogi', '1', '4'], ['gracz', '2']]},
+                          '2': [['koordynaty', '1100', '400'], ['drogi', '4', '4', '6', '9', '8'], ['gracz', '2']]},
                          mapka.wczytaj_txt('mapa_testowa.txt'))
 
     def test_wczytaj_domek(self):
         mapka = Mapa(10, 10, "nowa_mapa", 100)
-        slownik = mapka.wczytaj_txt('mapa_testowa.txt')
-        domek1 = Domek(1)
-        domek1.x = 300
-        domek1.y = 400
-        domek1.lista_sasiadow = [1, 6]
-        domek1.gracz = 1
-        domek2 = Domek(2)
-        domek2.x = 1100
-        domek2.y = 400
-        domek2.lista_sasiadow = [1, 4]
-        domek2.gracz = 2
-        self.assertEqual([domek1, domek2], list(mapka.wczytaj_domek(slownik)))
+        slowniczek = mapka.wczytaj_txt('mapa_testowa.txt')
+        lista = mapka.wczytaj_domki(slowniczek)
+        self.assertEqual((400, 1100 , [6], [4, 6, 9, 8]), (lista[1].x, lista[1].y, lista[0].lista_sasiadow, lista[1].lista_sasiadow))
+
+    def test_statystyki(self):
+        mapka = Mapa(10, 10, "nowa_mapa", 100)
+
+        """for i in
+        return "{}\n".format(self.prezentacja)"""
+
+    # Wypisać się w konsoli: plansza, drogi, statystyki
+    # (#domków, #graczy, odległość między graczami).

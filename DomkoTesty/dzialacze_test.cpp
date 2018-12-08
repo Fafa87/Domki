c8926eaf138c2dc77e11a3c42f52db187c184fd8
qwerty <fafafft@gmail.com>
2018-12-08 18:34:11 +0100

    #55: poprawa kompilacji i test

--- DomkoTesty/dzialacze_test.cpp
+++ DomkoTesty/dzialacze_test.cpp
@@ -1,4 +1,5 @@
 #include "CppUnitTest.h"
+#include <string>
 
 #include "../Domki/rozgrywka.h"
 #include "../Domki/dzialacze.h"
@@ -48,19 +49,32 @@ namespace DomkoTesty
 			Assert::AreNotEqual(domek_0.gracz->numer, domek_1.gracz->numer);
 			Assert::AreEqual(domek_0.liczebnosc, 1.0);
 		}
+
+		void SprawdzSilaGracza(int liczba, int produkcja, int kuznia, tuple<int,int,int> wyliczone)
+		{
+			Assert::AreEqual(liczba, get<0>(wyliczone));
+			Assert::AreEqual(produkcja, get<1>(wyliczone));
+			Assert::AreEqual(kuznia, get<2>(wyliczone));
+		}
+
 		TEST_METHOD(SilaGracza)
 		{
 			auto& domek_0 = tworca::DodajDomek(rozgrywka, 0, 10, PD());
 			tworca::DodajDomek(rozgrywka, 1, 30, PD());
 			tworca::DodajDomek(rozgrywka, 1, 7, PD());
-
-			Assert::AreEqual(10, rozgrywka.SilaGracza(0));
-			Assert::AreEqual(37, rozgrywka.SilaGracza(1));
-
+			
+			SprawdzSilaGracza(10, 1, 0, rozgrywka.SilaGracza(0));
+			SprawdzSilaGracza(37, 2, 0, rozgrywka.SilaGracza(1));
 			tworca::DodajLudka(rozgrywka, domek_0, 1, 12, PD());
 
-			Assert::AreEqual(10, rozgrywka.SilaGracza(0));
-			Assert::AreEqual(49, rozgrywka.SilaGracza(1));
+			SprawdzSilaGracza(10, 1, 0, rozgrywka.SilaGracza(0));
+			SprawdzSilaGracza(49, 2, 0, rozgrywka.SilaGracza(1));
+
+			domek_0.typdomku = TypDomku::kKuznia;
+			SprawdzSilaGracza(10, 0, 1, rozgrywka.SilaGracza(0));
+
+			domek_0.typdomku = TypDomku::kZamek;
+			SprawdzSilaGracza(10, 0, 0, rozgrywka.SilaGracza(0));
 		}
 
 		TEST_METHOD(Osy)

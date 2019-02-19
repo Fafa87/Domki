aa7d5376b5089452aa1e0190fb9780d82694f63b
qwerty <fafafft@gmail.com>
2019-02-19 18:50:46 +0100

    tymczasowe zmiany, aż wyprostujemy ścieżki kampanii

--- Domki/kampania.cpp
+++ Domki/kampania.cpp
@@ -13,6 +13,7 @@ Kampania::Kampania(string sciezka)
 	this->lista_plikow = get_all_names_within_folder(sciezka, false, true);
 	for (int i = 0; i < this->lista_plikow.size() / 2; i++)
 		this->lista_misji.push_back(this->lista_plikow[i]);
+
 	this->akt_misja = 1;
 }
 
@@ -22,10 +23,14 @@ Kampania::~Kampania()
 
 MisjaUstawienia Kampania::PobierzMisje(int numer)
 {
+	if (numer >= 2 && numer < 10)
+		numer += 2;
+	else if (numer >= 10)
+		numer = numer - 10 + 2;
 	MisjaUstawienia ustawienia;
 	ustawienia.grupa = "";
 	ustawienia.nazwa = this->lista_misji[numer - 1];
-	ustawienia.szybkosc = 1.3;
+	ustawienia.szybkosc = 1.5;
 	ustawienia.trudnosc = "podstawa";
 	ustawienia.walka_w_polu = 1;
 	ustawienia.do_ilu_wygranych = 1;
@@ -34,6 +39,10 @@ MisjaUstawienia Kampania::PobierzMisje(int numer)
 
 OpisMisji Kampania::PobierzOpis(int numer)
 {
+	if (numer >= 2 && numer < 10)
+		numer += 2;
+	else if (numer >= 10)
+		numer = numer - 10 + 2;
 	return OpisMisji(this->lista_plikow[numer - 1 + this->lista_plikow.size() / 2]);
 }
 
@@ -42,22 +51,7 @@ OpisMisji::OpisMisji(string sciezka)
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
 	ifstream plik;
-
-	std::ios_base::iostate exceptionMask = plik.exceptions() | std::ios::failbit;
-	plik.exceptions(exceptionMask);
-
-	try {
-		plik.open(sciezka);
-	}
-	catch (std::ios_base::failure& e) {
-		std::cout << "Caught an ios_base::failure.\n"
-			<< "Explanatory string: " << e.what() << '\n'
-			<< "Error code: " << e.code() << '\n';
-
-		std::fflush(stdout);
-	}
-
-	//cerr << "Error: " << strerror(errno);
+	plik.open(sciezka);
 
 	string tmp;
 	std::getline(plik, tmp);

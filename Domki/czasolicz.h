cbad393079dad70fcf4f04b9764a9c69e176ab7d
Gitomek <KolejnaNazwa>
2020-09-29 08:20:26 +0200

    Wstępny wygląd czasolicza

--- Domki/czasolicz.h
+++ Domki/czasolicz.h
@@ -12,10 +12,25 @@ class Czasolicz
         PING,
         OBLICZENIA
     };
+    Czasy czasy[4];
 
-
-    void init(enum czas) {}
-    void reset(enum czas) {}
-    void dodaj(enum czas, int ile, double czas) {}
-    double wart(enum czas) { return 0.0; }
+    void init(Czas ktory) {
+        int n = static_cast<int>(ktory);
+        czasy[n].ile = 0.0;
+        czasy[n].czas = 0.0;
+        czasy[n].sred = 0.0;
+    }
+    void reset(Czas ktory) {
+        int n = static_cast<int>(ktory);
+        czasy[n].ile = 0.0;
+        czasy[n].czas = 0.0;
+        czasy[n].sred = 0.0;
+    }
+    void dodaj(Czas ktory, int ile, double czas) {
+        int n = static_cast<int>(ktory);
+        czasy[n].ile += ile;
+        czasy[n].czas += czas;
+        czasy[n].sred = ile/czas;
+    }
+    double wart(Czas ktory) { return czasy[static_cast<int>(ktory)].sred; }
 };

78ed0d14b789a175d90067363de2e4c03480d062
Filip Mróz <fafafft@gmail.com>
2020-12-27 23:52:51 +0100

    skrót do uaktualnienia i to działającego #129

--- Paczka/Uaktualnij.ps1
+++ Paczka/Uaktualnij.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest "https://ci.appveyor.com/api/projects/fafa87/domki/artifacts/_Instalka%2Fbin%2Fx64%2FDeploy%2FDomcraft.msi?branch=master&job=Image%3A%20Visual%20Studio%202017" -OutFile Domcraft.msi
 
-msiexec /i Domcraft.msi REINSTALL=ALL REINSTALLMODE=vemus /passive
+msiexec /i Domcraft.msi /passive
 
 Remove-Item Domcraft.msi
